0808e158c2ccf5605d0efabd1d9eea97f4a3aec4
AxelSiwmark1 <siwmark@net.chalmers.se>
2023-02-06 19:03:59 +0100

    function to show complex numbers

--- Complex.hs
+++ Complex.hs
@@ -2,9 +2,13 @@ data ImagUnit = IU deriving Show
 i :: ImagUnit
 i = IU
 
-
+type REAL = Double
 -- a + bi or a + ib
 data CB where
     Plus1 :: REAL -> REAL -> ImagUnit -> CB
     Plus2 :: REAL -> ImagUnit -> REAL -> CB
  deriving (Show)
+-- Print complex number for ease of use
+showCB :: CB -> String
+showCB (Plus1 x y IU) = show x ++ "+" ++ show y ++ "i"
+showCB (Plus2 x IU y) = show x ++ "+" ++ "i" ++ show y
